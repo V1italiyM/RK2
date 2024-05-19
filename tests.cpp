7959096d7634da1fd86eed35e8abc2012dd2365f
V1taliyM <159753199+V1italiyM@users.noreply.github.com>
2024-05-19 21:32:20 +0300

    Add files via upload

---
+++ tests.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include "abstraction.h"
+#include "implementor.h"
+
+TEST(BridgeTest, ConcreteImplementorAOperation) {
+    ConcreteImplementorA implA;
+    RefinedAbstraction abs(&implA);
+    EXPECT_EQ(abs.Operation(), "ConcreteImplementorA Operation");
+}
+
+TEST(BridgeTest, ConcreteImplementorBOperation) {
+    ConcreteImplementorB implB;
+    RefinedAbstraction abs(&implB);
+    EXPECT_EQ(abs.Operation(), "ConcreteImplementorB Operation");
+}
+
+TEST(BridgeTest, ChangeImplementor) {
+    ConcreteImplementorA implA;
+    RefinedAbstraction abs(&implA);
+    EXPECT_EQ(abs.Operation(), "ConcreteImplementorA Operation");
+    ConcreteImplementorB implB;
+    abs.SetImplementor(&implB);
+    EXPECT_EQ(abs.Operation(), "ConcreteImplementorB Operation");
+}
