5c0ed41eb65809a54e04f126fa614e92a5d60e1d
V1taliyM <159753199+V1italiyM@users.noreply.github.com>
2024-05-19 21:52:11 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,23 +1,40 @@
 #include <gtest/gtest.h>
-#include "house_and_theme.cpp"
-#include "abstraction.h"
-#include "implementor.h"
+#include "bridge.cpp"
 
-TEST(BridgeTest, TestOperationA) {
-    ConcreteImplementorA implA;
+TEST(BridgeTest, InitialState) {
+    // Создание объектов
+    ImplementorA implA;
     RefinedAbstraction abs(&implA);
+
+    // Проверка начального состояния
     EXPECT_EQ(abs.Operation(), "ConcreteImplementorA: Operation");
 }
 
-TEST(BridgeTest, TestOperationB) {
-    ConcreteImplementorB implB;
-    RefinedAbstraction abs(&implB);
+TEST(BridgeTest, ChangeImplementor) {
+    // Создание объектов
+    ImplementorA implA;
+    RefinedAbstraction abs(&implA);
+    
+    // Смена реализации
+    ImplementorB implB;
+    abs.SetImplementor(&implB);
+    
+    // Проверка смены реализации
     EXPECT_EQ(abs.Operation(), "ConcreteImplementorB: Operation");
 }
 
-TEST(BridgeTest, TestChangeImplementor) {
-    ConcreteImplementorA implA;
+TEST(BridgeTest, ChangeToInitialImplementor) {
+    // Создание объектов
+    ImplementorA implA;
     RefinedAbstraction abs(&implA);
+
+    // Смена реализации
+    ImplementorB implB;
+    abs.SetImplementor(&implB);
+    
+    // Сброс к начальной реализации
     abs.SetImplementor(&implA);
+
+    // Проверка возвращения к начальной реализации
     EXPECT_EQ(abs.Operation(), "ConcreteImplementorA: Operation");
 }
